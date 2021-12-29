caa145da51850b9c98738e49efe4165af6b62edc
prgr <me@prgr.org>
2021-12-29 10:07:50 +0300

    read config

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/prgra/dhcptemplater
 go 1.17
 
 require (
+	github.com/BurntSushi/toml v0.4.1
 	github.com/jmoiron/sqlx v1.3.4
 )
--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/prgra/dhcptemplater/dhcp"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/prgra/dhcptemplater/dhcp"
+)
 
 func main() {
+	var conf dhcp.Cfg
+	_, err := toml.DecodeFile("dhcp.toml", &conf)
+	if err != nil {
+		log.Println("can't read config", err)
+		os.Exit(1)
+	}
+	fmt.Println(conf)
 	app := dhcp.NewApp(dhcp.Cfg{})
 	app.GetTemplates()
 }
